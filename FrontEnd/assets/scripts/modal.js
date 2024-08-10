ede072e8145196666cdb6f8a98e0b81b3c6c88ed
sarakdo <kdouh.sara@outlook.com>
2024-08-10 18:14:21 +0200

    mise en place des bouton de la modal

--- FrontEnd/assets/scripts/modal.js
+++ FrontEnd/assets/scripts/modal.js
@@ -1,13 +1,41 @@
+let modal = null; /* permet de fermet la modal */
+
 const openModal = function (e) {
   e.preventDefault();
-  const target = document.querySelector(e.target.getAttribut("href"));
+  const target = document.querySelector(e.target.getAttribute("href"));
   target.style.display = null;
   target.removeAttribute("aria-hidden");
   target.setAttribute("aria-modal", "true");
+  modal = target; /* on cible la target pour pouvoir la fermer  */
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .addEventListener("click", stopPropagation);
+};
+
+/* fermeture de la modal */
+const closeModal = function (e) {
+  if (modal === null) return;
+  e.preventDefault();
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+  modal.removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-close")
+    .removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .removeEventListener("click", stopPropagation);
+
+  modal = null;
+};
+/* evite que la page disparaisse lors d'un clique sur la page */
+const stopPropagation = function (e) {
+  e.stopPropagation();
 };
 
 document.querySelectorAll(".js-modal").forEach((a) => {
   a.addEventListener("click", openModal);
 });
-
-openModal();
