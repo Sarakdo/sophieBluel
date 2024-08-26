a7036d2189d4750d6d2bfade9456f9294e690171
sarakdo <kdouh.sara@outlook.com>
2024-08-26 12:03:45 +0200

    creation des clic de suppression des images

--- FrontEnd/assets/scripts/gallery.js
+++ FrontEnd/assets/scripts/gallery.js
@@ -19,14 +19,50 @@ async function showWorks(arrayWorks, myGallery) {
       figcaption.textContent = work.title;
       figure.appendChild(figcaption);
     } else {
+      // creation des boutons de poubelles
       const button = document.createElement("button");
       button.classList.add("fa-solid", "fa-trash-can", "delete-btn");
+      const buttonDelete = document.createElement("div");
+      buttonDelete.classList.add("button-wrap");
+      buttonDelete.appendChild(button);
       figure.style.position = "relative";
-      figure.appendChild(button);
+      figure.appendChild(buttonDelete);
     }
     myGallery.appendChild(figure);
   });
 }
+
+// Ajout des cliques de chaque élément de la poubelle
+const deleteButtons = document.querySelectorAll(".delete-btn");
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", async () => {
+    const figure = button.closest("figure");
+    const imageUrl = figure.querySelector("img").src;
+    // Supprimer le work correspondant à l'image
+    const response = await fetch("http://localhost:5678/api/works/", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ imageUrl: imageUrl }),
+    });
+
+    if (response.ok) {
+      figure.remove();
+    } else {
+      // Gérer l'erreur
+      console.log("Erreur lors de la suppression du work");
+    }
+  });
+});
+// creation du clic ajout d'image
+/* 
+const upload = document.querySelector(".btn-upload");
+upload.addEventListener("click", () => {
+  upload.classList.add("");
+  });
+  });
+  */
 // Creation d'un message d'erreur dans le cas d'une erreur survenue//
 const errorHandling = (error) => {
   const div = document.createElement("div");
